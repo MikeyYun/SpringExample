a1df3a273293231255ba68435c5b05f45069ff2e
엄시윤 <happiness010108@gmail.com>
2024-11-12 21:07:15 +0700

    [엄시윤] - mvc cycle 예제 풀이

---
+++ src/main/java/com/mikeyyun/spring/ex/mvc/domain/User.java
@@ -0,0 +1,60 @@
+package com.mikeyyun.spring.ex.mvc.domain;
+
+import java.time.LocalDateTime;
+
+public class User {
+	
+	private int id;
+	private String name;
+	private String yymmdd;
+	private String email;
+	private String introduce;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getYymmdd() {
+		return yymmdd;
+	}
+	public void setYymmdd(String yymmdd) {
+		this.yymmdd = yymmdd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getIntroduce() {
+		return introduce;
+	}
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
+
+}
